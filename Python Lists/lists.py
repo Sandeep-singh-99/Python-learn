02c78d794e78c6f547037375876de6e9da51de72
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2026-02-10 21:09:47 +0530

    Add lists.py with examples for list operations and methods

---
+++ Python Lists/lists.py
@@ -0,0 +1,62 @@
+# thisList = ["apple", "banana", "cherry"]
+# print(thisList)
+
+# thisList = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thisList)
+# print(len(thisList))
+# print(type(thisList))
+
+# list1 = ["apple", "banana", "cherry"]
+# list2 = [1, 5, 7, 9, 3]
+# list3 = [True, False, False]
+# list4 = ["abc", 34, True, 40, "male"]
+# print(list1)
+# print(list2)
+# print(list3)
+# print(list4)
+
+# Accessing list items
+
+# thisList = ["apple", "banana", "cherry"]
+# print(thisList[-1])
+# print(thisList[0:2])
+# print(thisList[:2])
+# print(thisList[2:])
+# print(thisList[-4:-1])
+# print(thisList[0:0])
+
+# if "apple" in thisList:
+#     print("Yes, 'apple' is in the fruits list")
+# else:
+#     print("No, 'apple' is not in the fruits list")
+
+# Change Item Value
+
+# thisList = ["apple", "banana", "cherry"]
+# thisList[1] = "blackcurrant"
+# print(thisList)
+
+# Add List Items
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+
+# thislist.insert(1, "lemon")
+# print(thislist)
+
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+
+"""
+The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
+"""
+
+# thisTuple = ("kiwi", "watermelon")
+# thislist.extend(thisTuple)
+# print(thislist)
+
+
+# Remove List Items
+
